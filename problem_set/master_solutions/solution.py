406692572d35731f5219a8b7ef2daf4373b5f5a6
a1ef0 <a.buchnev@innopolis.university>
2023-04-29 11:38:55 +0300

    modify solution template && add config files

--- problem_set/master_solutions/solution.py
+++ problem_set/master_solutions/solution.py
@@ -1,12 +1,4 @@
 class Solution:
-    tests_no: int
-    last_generated_test: str
-    task_id: int
-
-    def __init__(self):
-        """Instantiates the Solution class. Any child class must set the number of tests (tests_no) and task id (task_id).
-        """
-        pass
 
     def gen_sample(self):
         """Generates a single sample of valid test data and prints it.
--- problem_set/master_solutions/task_1.py
+++ problem_set/master_solutions/task_1.py
@@ -5,33 +5,26 @@ import argparse
 
 class Task1(Solution):
 
-    tests_no: int
-    last_generated_test: str
-    task_id: int
-
-    def __init__(self):
-        self.tests_no = 100
-        self.task_id = 1
-
     # @override
     def gen_sample(self):
-        LOWER_BOUNDARY = int(-1e9)
-        UPPER_BOUNDARY = int(1e9)
+        LOWER_BOUNDARY = int(-1e1)
+        UPPER_BOUNDARY = int(1e1)
         a = randint(LOWER_BOUNDARY, UPPER_BOUNDARY)
         b = randint(LOWER_BOUNDARY, UPPER_BOUNDARY)
         sample_input = f"{a} {b}"
         print(sample_input)
 
+    # @override
     def run(self):
         print(sum(map(int, input().split())))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", type=int)
+    parser.add_argument("mode")
     mode = parser.parse_args().mode
     task = Task1()
-    if mode == 0:
+    if mode == 'sample':
         task.gen_sample()
     else:
         task.run()
