60e92078d8063b09023822c51ef355952ad69146
qexik <qexik@hotmail.com>
2023-07-03 20:38:21 +0300

    Add method for getting role

--- frontend/lib/repositories/user_repository.dart
+++ frontend/lib/repositories/user_repository.dart
@@ -1,5 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/user.dart';
+import 'package:http/http.dart' as http;
+import 'package:frontend/env/config.dart';
 
 class UserRepository {
   UserRepository(this.prefs) {
@@ -23,5 +25,17 @@ class UserRepository {
       _user = User(jwt);
       prefs.setString("jwt", jwt);
     }
+  }
+
+  Future<String> getRole() async {
+    if (user?.jwt == null) {
+      return "Role.guest";
+    }
+    var response = await http.get(Uri.parse("$host/check_privileges"),
+        headers: <String, String>{
+          "Authorization": _user!.jwt,
+          "Content-Type": "application/json"
+        },);
+    return response.body;
   }
 }
