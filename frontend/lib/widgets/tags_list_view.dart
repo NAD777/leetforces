68aa62cfc600e6e914391810a9a28938dd842fc9
Vladislav <v.danshov@innopolis.university>
2023-07-07 21:46:40 +0300

    add tags for admincontest

--- frontend/lib/widgets/tags_list_view.dart
+++ frontend/lib/widgets/tags_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class TagsListView extends StatelessWidget {
   const TagsListView({
@@ -6,15 +7,17 @@ class TagsListView extends StatelessWidget {
     required this.tags,
     required this.isAdmin,
     this.onDelete,
+    this.onCreate,
   });
 
   final List<String> tags;
   final bool isAdmin;
   final Function(int)? onDelete;
+  final Function(String)? onCreate;
 
   @override
   Widget build(BuildContext context) {
-    assert((isAdmin && onDelete != null) || !isAdmin);
+    assert((isAdmin && onDelete != null && onCreate != null) || !isAdmin);
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
       const Text('Tags: '),
       ...List<Widget>.generate(
@@ -30,6 +33,55 @@ class TagsListView extends StatelessWidget {
               : null,
         ),
       ),
+      if (isAdmin)
+        ActionChip(
+          label: const Text('Add'),
+          avatar: const Icon(Icons.add),
+          onPressed: () {},
+        ),
     ]);
+  }
+
+  Future<void> askForName(BuildContext context) async {
+    var controller = TextEditingController();
+
+    var dialog = AlertDialog(
+      title: const Text('Enter tag name'),
+      content: TextField(
+        controller: controller,
+        decoration: const InputDecoration(
+          hintText: "Name for tag",
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => context.pop(),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            if (controller.text != '') {
+              onCreate!(controller.text);
+              context.pop();
+            } else {
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                  const SnackBar(
+                    content: Text('You cannot create tag with empty name'),
+                  ),
+                );
+            }
+          },
+          child: const Text('Create'),
+        ),
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (context) {
+        return dialog;
+      },
+    );
   }
 }
