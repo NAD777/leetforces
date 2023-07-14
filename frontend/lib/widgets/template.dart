fc08c3b44d0e32ca76aedb8925c5f9380278b5ef
Vladislav <v.danshov@innopolis.university>
2023-07-14 20:39:02 +0300

    modify template.dart with admin checking

--- frontend/lib/widgets/template.dart
+++ frontend/lib/widgets/template.dart
@@ -3,11 +3,44 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/repositories/user_repository.dart';
 import 'package:go_router/go_router.dart';
 
-class Template extends StatelessWidget {
-  const Template({required this.content, super.key, this.scrollable = true});
+class Template extends StatefulWidget {
+  const Template({
+    required this.content,
+    super.key,
+    this.scrollable = true,
+    this.isAdminPage = false,
+    this.onFabPressed,
+  });
 
   final Widget content;
   final bool scrollable;
+  final bool isAdminPage;
+  final Function()? onFabPressed;
+
+  @override
+  State<Template> createState() => _TemplateState();
+}
+
+class _TemplateState extends State<Template> {
+  bool isAdmin = false;
+  bool inProgress = true;
+
+  @override
+  void initState() {
+    super.initState();
+    RepositoryProvider.of<UserRepository>(context).getRole().then((value) {
+      if (value == "Role.admin" || value == "Role.superAdmin") {
+        setState(() {
+          isAdmin = true;
+        });
+      } else if (widget.isAdminPage) {
+        context.go('/');
+      }
+      setState(() {
+        inProgress = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +48,18 @@ class Template extends StatelessWidget {
 
     var padding = Padding(
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-      child: Center(child: content),
+      child: Center(child: widget.content),
     );
+
+    var body = widget.scrollable
+        ? SingleChildScrollView(
+            child: padding,
+          )
+        : padding;
 
     return Scaffold(
       appBar: AppBar(
-        surfaceTintColor: !scrollable ? Colors.transparent : null,
+        surfaceTintColor: !widget.scrollable ? Colors.transparent : null,
         title: GestureDetector(
             child: const Text('LeetForces'),
             onTap: () {
@@ -28,6 +67,13 @@ class Template extends StatelessWidget {
             }),
         actions: user != null
             ? <IconButton>[
+                if (isAdmin)
+                  IconButton(
+                    onPressed: () {
+                      context.go('/admin/tag');
+                    },
+                    icon: const Icon(Icons.tag),
+                  ),
                 IconButton(
                   onPressed: () {
                     context.go("/profile");
@@ -54,11 +100,15 @@ class Template extends StatelessWidget {
                 ),
               ],
       ),
-      body: scrollable
-          ? SingleChildScrollView(
-              child: padding,
+      body: widget.isAdminPage && inProgress
+          ? const Center(child: CircularProgressIndicator())
+          : body,
+      floatingActionButton: widget.onFabPressed != null
+          ? FloatingActionButton(
+              onPressed: widget.onFabPressed,
+              child: const Icon(Icons.add),
             )
-          : padding,
+          : null,
     );
   }
 }
